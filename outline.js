05f1a9bb83c99e25648b5fbde2bbece07b59bb99
William Lu <william.lu@shopify.com>
2021-03-08 19:43:21 -0500

    Remove onInstall event listener

--- outline.js
+++ outline.js
@@ -1,28 +1,27 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
-    if (command === 'open_outline') {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        tab = tabs[0];
-        id = tab.id;
-        url = tab.url;
-        console.log(url);
-        chrome.tabs.update(id, {url: 'https://outline.com/'}, function() {
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('Command:', command);
+  if (command === 'open_outline') {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      tab = tabs[0];
+      id = tab.id;
+      url = tab.url;
+      console.log(url);
+      chrome.tabs.update(id, {url: 'https://outline.com/'}, function() {
 
-          listener = function(tabId, changeInfo, tab) {
-            if (tabId == tab.id && changeInfo.status === 'complete') {
-              chrome.storage.local.set({
-                url: url
-              }, function() {
-                chrome.tabs.executeScript(id, {
-                  file: 'outline_dom.js'
-                });
+        listener = function(tabId, changeInfo, tab) {
+          if (tabId == tab.id && changeInfo.status === 'complete') {
+            chrome.storage.local.set({
+              url: url
+            }, function() {
+              chrome.tabs.executeScript(id, {
+                file: 'outline_dom.js'
               });
-            }
+            });
+            chrome.tabs.onUpdated.removeListener(listener)
           }
-          chrome.tabs.onUpdated.addListener(listener)
-        });
+        }
+        chrome.tabs.onUpdated.addListener(listener)
       });
-    }
-  });
+    });
+  }
 });
